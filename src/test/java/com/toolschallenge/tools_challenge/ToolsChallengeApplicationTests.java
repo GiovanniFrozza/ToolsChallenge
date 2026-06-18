66d938cff454c875b77a59c698709714e066fe2d
Giovanni Frozza <giovannifrozza470@gmail.com>
2026-06-17 21:59:09 -0300

    Spring Boot configurações iniciais

---
+++ src/test/java/com/toolschallenge/tools_challenge/ToolsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolschallenge.tools_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
